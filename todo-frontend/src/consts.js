4d7821543650fe13c1fecec1dea070aea88b99d2
Dennis Rizah Nordvi Osmani <dennis.osmani@no.experis.com>
2024-10-11 11:27:41 +0200

    done

--- todo-frontend/src/consts.js
+++ todo-frontend/src/consts.js
@@ -1,6 +1,6 @@
 import "react";
 
-const url = import.meta.env.VITE_ENDPOINT_URL || "http://localhost:5168";
+const url = import.meta.env.VITE_ENDPOINT_URL || "http://aws-day-1-dennisosmani-api-env-1.eba-xk2mds4f.eu-north-1.elasticbeanstalk.com/";
 
 // ensure trailing / is removed
 export const API_URL = url.replace(/\/$/, "");
